1791571688eed2b3509394ca3bcee5240ae8d1f6
juanclawav <juanclaudiocarrascotapia@gmail.com>
2024-11-05 23:37:43 -0400

    commit quitar algunas cositas

--- ai_assistant/rags.py
+++ ai_assistant/rags.py
@@ -6,7 +6,6 @@ from llama_index.core import SimpleDirectoryReader, StorageContext
 from ai_assistant.prompts import qa_template
 from llama_index.core.indices import MultiModalVectorStoreIndex
 from ai_assistant.config import Config
-from qdrant_client.http.exceptions import UnexpectedResponse
 import qdrant_client
 
 # Set up OpenAI LLM
@@ -15,38 +14,17 @@ openai_mm_llm = OpenAIMultiModal(model="gpt-4o", api_key=Config.OPENAI_API_KEY)
 
 # Initialize Qdrant vector stores for text and image data
 client = qdrant_client.QdrantClient(path="qdrant_mm_db")
-
-def collection_exists(client, collection_name):
-    try:
-        client.get_collection(collection_name)
-        return True
-    except UnexpectedResponse:
-        return False
-
-# Check if collections already exist in Qdrant
-text_collection_exists = collection_exists(client, "text_collection")
-# Initialize Qdrant vector stores for text and image data
-if text_collection_exists:
-    print("Reusing existing text collection in Qdrant.")
-else:
-    print("Creating a new text collection in Qdrant.")
-
-
-# Create or reuse the vector stores
 text_store = QdrantVectorStore(client=client, collection_name="text_collection")
+image_store = QdrantVectorStore(client=client, collection_name="image_collection")
 
-storage_context = StorageContext.from_defaults(vector_store=text_store)
+storage_context = StorageContext.from_defaults(vector_store=text_store, image_store=image_store)
 
-# Only build the index if collections do not already exist
-if not text_collection_exists:
-    documents = SimpleDirectoryReader(str(Config.DATA_PATH)).load_data()
-    index = MultiModalVectorStoreIndex.from_documents(
-        documents,
-        storage_context=storage_context,
-    )
-else:
-    # Load existing index without rebuilding
-    index = MultiModalVectorStoreIndex(storage_context=storage_context)
+# Build the multimodal index
+documents = SimpleDirectoryReader(str(Config.DATA_PATH)).load_data()
+index = MultiModalVectorStoreIndex.from_documents(
+    documents,
+    storage_context=storage_context,
+)
 
 # Query Engine
 query_engine = index.as_query_engine(
